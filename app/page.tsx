5295b736c5fc22d2c13b6da178d1446ba84b002e
Dairou Wang <dairou2021@gmail.com>
2024-06-27 01:17:15 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container">
+        <h1 className= "title">Pet MBTI</h1>
+        <p className = "intro">What is your pet's MBTI? Discover their personality traits and how to best understand and care for them.</p>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://www.google.com"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="start button">
+            Start{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </a>
+
+      </div>
+    </main>
+  );
+}
